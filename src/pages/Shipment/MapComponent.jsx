da2fb009c3a2498859650f733acf9a986d1dfea7
Pradosh <mfsi.pradoshs@gmail.com>
2024-07-24 21:29:54 +0530

    Added marker.png

--- src/pages/Shipment/MapComponent.jsx
+++ src/pages/Shipment/MapComponent.jsx
@@ -6,7 +6,16 @@ import {
   Popup,
   TileLayer,
 } from 'react-leaflet';
+import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
+import icon from 'leaflet/dist/images/marker-icon.png';
+import iconShadow from 'leaflet/dist/images/marker-shadow.png';
+
+let DefaultIcon = L.icon({
+  iconUrl: icon,
+  shadowUrl: iconShadow,
+});
+L.Marker.prototype.options.icon = DefaultIcon;
 
 import { Alert } from '@mui/material';
 import { getShipmentRoute } from './shipmentApi';
